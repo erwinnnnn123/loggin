52764915a629d6e1d33bd562ff253cf4fc7efca5
Erwin <156868182+erwinnnnn123@users.noreply.github.com>
2024-01-17 10:17:58 +0700

    Add files via upload

---
+++ afterlogin.php
@@ -0,0 +1,9 @@
+<html>
+<head><title>status login</title></head>
+<body>
+	status login anda :<br>
+	<?php
+	echo $reslt;
+	?>
+</body>
+</html>
---
+++ login.php
@@ -0,0 +1,25 @@
+<html>
+<head><b>silahkan login untuk masuk ke dalam sistem</b></head>
+<body>
+	<?php
+	echo $reslt;
+	?>
+	<form action="" method="POST">
+		<p>
+			<label>Username</label>
+			<input id="username" value="" name="username" type="text" required="required" /><br>
+		</p>
+
+		<p>
+			<label>Password</label>
+			<input id="password" name="password" type="password" required="required" />
+		</p>
+	<br />
+	<p>
+		<button type="submit" name="submit"><span>login</span></button>
+		<button type="reset"><span>reset</span></button>
+	</p>
+</form>
+</body>
+</body>
+</html>
